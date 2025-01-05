68552f8fe13306bb9fbd64055110d712d02f4df0
Prajesh24 <149761502+Prajesh24@users.noreply.github.com>
2025-01-05 12:35:59 +0545

    sprint 3 completed

--- app/src/main/java/com/example/datemate_sd/ui/activity/ProfileDetailsActivity.kt
+++ app/src/main/java/com/example/datemate_sd/ui/activity/ProfileDetailsActivity.kt
@@ -3,6 +3,8 @@ package com.example.datemate_sd.ui.activity
 import android.app.DatePickerDialog
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,9 +13,11 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.datemate_sd.R
 import com.example.datemate_sd.databinding.ActivityProfileDetailsBinding
 
-class ProfileDetailsActivity : AppCompatActivity() {
+class ProfileDetailsActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     lateinit var binding:ActivityProfileDetailsBinding
+
+    var cities= arrayOf("Kathmandu","Bhaktapur","Lalitpur","Pokhara","Butwal","Janakpur","Dharan","Mahendranagar","Biratnagar")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +26,21 @@ class ProfileDetailsActivity : AppCompatActivity() {
         binding=ActivityProfileDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.dateInput.isClickable=true
         binding.dateInput.isFocusable=false
         binding.dateInput.setOnClickListener {
             loadCalender()
         }
+
+        val cityAdapter = ArrayAdapter(
+            this@ProfileDetailsActivity,
+            android.R.layout.simple_dropdown_item_1line,
+            cities
+        )
+        binding.addressSpinner.adapter = cityAdapter
+        binding.addressSpinner.onItemSelectedListener = this
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -49,6 +63,7 @@ class ProfileDetailsActivity : AppCompatActivity() {
 
         dialog.show()
     }
+
 
 
 }
