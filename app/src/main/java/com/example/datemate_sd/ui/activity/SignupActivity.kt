807040d3c9aba1bcf4871be1561928e18cdece49
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-01-10 13:36:40 +0545

    Changes on signin Activity

--- app/src/main/java/com/example/datemate_sd/ui/activity/SignupActivity.kt
+++ app/src/main/java/com/example/datemate_sd/ui/activity/SignupActivity.kt
@@ -1,17 +1,29 @@
 package com.example.datemate_sd.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.datemate_sd.R
+import com.example.datemate_sd.databinding.ActivityLoginBinding
+import com.example.datemate_sd.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.BackTOLogin.setOnClickListener{
+            val intent = Intent(this@SignupActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
