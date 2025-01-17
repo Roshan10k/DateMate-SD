50ea8af158e9e02b8a0484f2c2396844570452d7
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-01-17 22:04:31 +0545

    Adding all changes

--- app/src/main/java/com/example/datemate_sd/ui/activity/NavigationActivity.kt
+++ app/src/main/java/com/example/datemate_sd/ui/activity/NavigationActivity.kt
@@ -26,7 +26,6 @@ class NavigationActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         replaceFragment(DashboardFragment())
-
         binding.navigationView.setOnItemSelectedListener{menu ->
             when(menu.itemId){
                 R.id.navHome -> replaceFragment(DashboardFragment())
@@ -35,6 +34,7 @@ class NavigationActivity : AppCompatActivity() {
             }
             true
         }
+
        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
