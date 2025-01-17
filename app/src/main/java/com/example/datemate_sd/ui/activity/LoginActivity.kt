e769b90c6922211daa90290745d5af5eb7df32d4
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-01-17 22:17:32 +0545

    adding changes in login and forget password

--- app/src/main/java/com/example/datemate_sd/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/datemate_sd/ui/activity/LoginActivity.kt
@@ -29,6 +29,12 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.forgetbtn.setOnClickListener{
+            val intent = Intent(this@LoginActivity, ForgetActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
