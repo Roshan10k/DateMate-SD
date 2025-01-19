ca5ad1de06b3f0e00ffc3fcc1311449fa2f08a64
Roshan Khadka <khadkaasd963@gmail.com>
2025-01-19 18:09:59 +0545

    firebase connected

--- app/src/main/java/com/example/datemate_sd/model/UserModel.kt
+++ app/src/main/java/com/example/datemate_sd/model/UserModel.kt
@@ -1,4 +1,12 @@
 package com.example.datemate_sd.model
 
-class UserModel {
+data class UserModel(
+    var UserId : String ="",
+    var name: String = "",
+    var emailAddress:String ="",
+    var phnNumber : String="",
+    var dateOfBirth: String="",
+    var address: String=""
+) {
+
 }
