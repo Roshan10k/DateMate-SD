e36d86414c534f6a458d8aa2d450f18de22ee48f
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2025-01-23 13:11:38 +0545

    Added like, message and return functions/Buttons

--- app/src/main/java/com/example/datemate_sd/ui/activity/ProfilePageActivity.kt
+++ app/src/main/java/com/example/datemate_sd/ui/activity/ProfilePageActivity.kt
@@ -1,30 +1,58 @@
 package com.example.datemate_sd.ui.activity
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.datemate_sd.R
+import com.example.datemate_sd.databinding.ActivityForgetBinding
+import com.example.datemate_sd.databinding.ActivityProfilePageBinding
 
 class ProfilePageActivity : AppCompatActivity() {
-
-
-
+    lateinit var binding: ActivityProfilePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_page)
+        enableEdgeToEdge()
+        binding = ActivityProfilePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.backBtn.setOnClickListener{
+            val intent = Intent(this@ProfilePageActivity, NavigationActivity::class.java)
+            startActivity(intent)
 
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.image1)) { view, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
-    }}
 
+
+        binding.msgBtn.setOnClickListener{
+            val intent = Intent(this@ProfilePageActivity, NavigationActivity::class.java)
+            intent.putExtra("navigate_to", "MessageFragment")
+            startActivity(intent)
+
+        }
+
+        var isLiked = false
+
+        binding.likeBtn.setOnClickListener {
+            if (isLiked) {
+                binding.likeBtn.imageTintList =
+                    ColorStateList.valueOf(ContextCompat.getColor(this, R.color.white))
+                Toast.makeText(this, "Unliked!", Toast.LENGTH_SHORT).show()
+            } else {
+                binding.likeBtn.imageTintList =
+                    ColorStateList.valueOf(ContextCompat.getColor(this, R.color.endpink))
+                Toast.makeText(this, "Like Sent", Toast.LENGTH_SHORT).show()
+            }
+            isLiked = !isLiked
+        }
+
+
+    }
+}
 
 
