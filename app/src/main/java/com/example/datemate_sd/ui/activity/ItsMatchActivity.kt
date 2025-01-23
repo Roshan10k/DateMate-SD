2702bec2388fa647ce8a7014c8697b383653e4dc
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2025-01-23 11:51:07 +0545

    creating a Its match activity

--- app/src/main/java/com/example/datemate_sd/ui/activity/ItsMatchActivity.kt
+++ app/src/main/java/com/example/datemate_sd/ui/activity/ItsMatchActivity.kt
@@ -1,30 +1,21 @@
 package com.example.datemate_sd.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.datemate_sd.R
 
-class ProfilePageActivity : AppCompatActivity() {
-
-
-
+class ItsMatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_page)
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.image1)) { view, insets ->
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_its_match)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }}
-
-
-
+    }
+}
