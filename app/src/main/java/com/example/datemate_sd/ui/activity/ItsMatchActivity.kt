99b749a7ece794d80047cee87d48c9d6cda5b084
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2025-01-23 11:53:13 +0545

    returning button functionality

--- app/src/main/java/com/example/datemate_sd/ui/activity/ItsMatchActivity.kt
+++ app/src/main/java/com/example/datemate_sd/ui/activity/ItsMatchActivity.kt
@@ -1,17 +1,27 @@
 package com.example.datemate_sd.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.datemate_sd.R
+import com.example.datemate_sd.databinding.ActivityItsMatchBinding
 
 class ItsMatchActivity : AppCompatActivity() {
+    lateinit var binding: ActivityItsMatchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_its_match)
+        binding=ActivityItsMatchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.back.setOnClickListener{
+            val intent = Intent(this@ItsMatchActivity, NavigationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
