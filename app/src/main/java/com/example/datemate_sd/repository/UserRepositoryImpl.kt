6fa91c4b74fa904794903077dc981e059459e3b8
Roshan Khadka <khadkaasd963@gmail.com>
2025-01-28 18:39:32 +0545

    creating userrepo implementation for login,register,logout and forget password

--- app/src/main/java/com/example/datemate_sd/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/datemate_sd/repository/UserRepositoryImpl.kt
@@ -1,11 +1,23 @@
 package com.example.datemate_sd.repository
 
 import com.example.datemate_sd.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 
 class UserRepositoryImpl:UserRepository {
+    var auth:FirebaseAuth = FirebaseAuth.getInstance()
+    var database:FirebaseDatabase=FirebaseDatabase.getInstance()
+    var reference = database.reference.child("users")
+
     override fun login(email: String, password: String, callback: (Boolean, String) -> Unit) {
-        TODO("Not yet implemented")
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+            if (it.isSuccessful){
+                callback(true,"Login successful")
+            }else{
+                callback(false,it.exception?.message.toString())
+            }
+        }
     }
 
     override fun register(
@@ -13,11 +25,23 @@ class UserRepositoryImpl:UserRepository {
         password: String,
         callback: (Boolean, String, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+            if (it.isSuccessful){
+                callback(true,"Registration succesful",auth.currentUser?.uid.toString())
+            }else{
+                callback(false,it.exception?.message.toString(),"")
+            }
+        }
     }
 
     override fun forgetPassword(email: String, callback: (Boolean, String) -> Unit) {
-        TODO("Not yet implemented")
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+            if (it.isSuccessful){
+                callback(true,"Password reset link sent to $email ")
+            }else{
+                callback(false,it.exception?.message.toString())
+            }
+        }
     }
 
     override fun addUserToDatabase(
@@ -29,7 +53,13 @@ class UserRepositoryImpl:UserRepository {
     }
 
     override fun logout(callback: (Boolean, String) -> Unit) {
-        TODO("Not yet implemented")
+        try {
+            auth.signOut()
+            callback(true,"Logout successful")
+
+        }catch (e:Exception){
+            callback(false,e.message.toString())
+        }
     }
 
     override fun editProfile(
