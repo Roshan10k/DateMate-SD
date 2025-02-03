cddb08ec7716eab96dd686923e760e33f1c0353b
Roshan Khadka <khadkaasd963@gmail.com>
2025-02-03 21:15:35 +0545

    login authorisation completed

--- app/src/main/java/com/example/datemate_sd/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/datemate_sd/ui/activity/LoginActivity.kt
@@ -2,20 +2,30 @@ package com.example.datemate_sd.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.datemate_sd.R
 import com.example.datemate_sd.databinding.ActivityLoginBinding
+import com.example.datemate_sd.repository.UserRepositoryImpl
+import com.example.datemate_sd.viewmodel.UserViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     lateinit var  binding: ActivityLoginBinding
+
+     lateinit var  userViewModel: UserViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val repo = UserRepositoryImpl()
+        userViewModel = UserViewModel(repo)
 
         binding.SignupBtn.setOnClickListener{
             val intent = Intent(this@LoginActivity, SignupActivity::class.java)
@@ -24,9 +34,29 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.loginButton.setOnClickListener{
-            val intent = Intent(this@LoginActivity, NavigationActivity::class.java)
-            startActivity(intent)
-            finish()
+            val email = binding.EmailView.text.toString()
+            val password = binding.passwordView.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            userViewModel.login(email, password) { success, message ->
+
+                if (success) {
+                    Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@LoginActivity,NavigationActivity::class.java)
+                    startActivity(intent)
+
+
+
+
+                } else {
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                }
+            }
+
         }
 
         binding.forgetbtn.setOnClickListener{
