d18c5928845d5df57ab53b7202728f0a75e31392
Roshan Khadka <khadkaasd963@gmail.com>
2025-02-14 20:54:14 +0545

    fetching curent user data to setting fragment

--- app/src/main/java/com/example/datemate_sd/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/example/datemate_sd/ui/fragment/SettingFragment.kt
@@ -6,9 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.datemate_sd.R
+import com.example.datemate_sd.databinding.FragmentSettingBinding
+import com.example.datemate_sd.repository.UserRepositoryImpl
+import com.example.datemate_sd.viewmodel.UserViewModel
+import com.squareup.picasso.Picasso
 
 
 class SettingFragment : Fragment() {
+    lateinit var  binding: FragmentSettingBinding
+    lateinit var  userViewModel: UserViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +27,31 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        binding = FragmentSettingBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var repo = UserRepositoryImpl()
+        userViewModel = UserViewModel(repo)
+
+        var currentUser = userViewModel.getCurrentUser()
+
+        currentUser.let { 
+            userViewModel.getUserFromDatabase((it?.uid.toString()))
+
+        }
+
+        userViewModel.users.observe(requireActivity()){
+            binding.nameSetting.text = it?.name.toString()
+            binding.userEmail.text = it?.emailAddress.toString()
+            Picasso.get().load(it?.imageurl).placeholder(R.drawable.baseline_person_24)
+                .into(binding.settingProfileImage)
+
+
+        }
     }
 
 
