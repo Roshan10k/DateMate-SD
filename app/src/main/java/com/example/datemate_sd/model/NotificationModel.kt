419e794e295509296ada7e7822dbf261f4d42bbc
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-02-17 15:18:12 +0545

    Adding the accessToken for the Notification

--- app/src/main/java/com/example/datemate_sd/model/NotificationModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.datemate_sd.model
-
-data class NotificationModel(
-    val title: String = "",
-    val description: String = "",
-    val timestamp: Long = System.currentTimeMillis()
-)
