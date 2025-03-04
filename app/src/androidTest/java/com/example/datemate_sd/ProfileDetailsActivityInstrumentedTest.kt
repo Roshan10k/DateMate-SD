b1da6b56fb2e2d354f6a5397e3dacfc0711767d4
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2025-03-04 21:10:10 +0545

    Instrumented Profiledetails setup

---
+++ app/src/androidTest/java/com/example/datemate_sd/ProfileDetailsActivityInstrumentedTest.kt
@@ -0,0 +1,35 @@
+package com.example.datemate_sd
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.datemate_sd.ui.activity.ProfileDetailsActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ProfileDetailsActivityInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(ProfileDetailsActivity::class.java)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
