39a294d60d4c2df3a2fae10021487578e1babd77
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2025-03-04 20:48:28 +0545

    Instrumented Ideal match case1

--- app/src/androidTest/java/com/example/datemate_sd/IdealMatchPageInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/datemate_sd/IdealMatchPageInstrumentedTest.kt
@@ -1,4 +1,39 @@
 package com.example.datemate_sd
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.datemate_sd.ui.activity.IdealMatchPage
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
 class IdealMatchPageInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(IdealMatchPage::class.java)
+
+    @Test
+    fun testBackArrowClick() {
+        onView(withId(R.id.backArrow))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
