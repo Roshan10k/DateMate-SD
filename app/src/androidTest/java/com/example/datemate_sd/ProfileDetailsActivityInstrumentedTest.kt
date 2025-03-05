fd2bb29d6325182d53ffb3e90049d60eb852431d
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2025-03-05 17:14:33 +0545

    finalized

--- app/src/androidTest/java/com/example/datemate_sd/ProfileDetailsActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/datemate_sd/ProfileDetailsActivityInstrumentedTest.kt
@@ -8,6 +8,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.datemate_sd.ui.activity.ProfileDetailsActivity
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,104 +21,63 @@ class ProfileDetailsActivityInstrumentedTest {
     val activityRule = ActivityScenarioRule(ProfileDetailsActivity::class.java)
 
     @Test
-    fun testBackButtonClick() {
-        onView(withId(R.id.backBtn))
-            .check(matches(isDisplayed()))
-            .perform(click())
-
+    fun testAllElementsDisplayed() {
+        // Check if all key elements are displayed
+        onView(withId(R.id.addProfileHead1)).check(matches(isDisplayed()))
+        onView(withId(R.id.addProfileHead2)).check(matches(isDisplayed()))
+        onView(withId(R.id.profileImg)).check(matches(isDisplayed()))
+        onView(withId(R.id.ImageBtn)).check(matches(isDisplayed()))
+        onView(withId(R.id.NameInput)).check(matches(isDisplayed()))
+        onView(withId(R.id.UsernameInput)).check(matches(isDisplayed()))
+        onView(withId(R.id.PhoneInput)).check(matches(isDisplayed()))
+        onView(withId(R.id.DateInput)).check(matches(isDisplayed()))
+        onView(withId(R.id.AddressSpinner)).check(matches(isDisplayed()))
+        onView(withId(R.id.continueBtnPD)).check(matches(isDisplayed()))
     }
 
     @Test
     fun testTitleAndSubtitleDisplay() {
         onView(withId(R.id.addProfileHead1))
-            .check(matches(isDisplayed()))
             .check(matches(withText("Add Profile Details")))
 
         onView(withId(R.id.addProfileHead2))
-            .check(matches(isDisplayed()))
             .check(matches(withText("Please add your profile details here")))
     }
 
     @Test
-    fun testProfileImageEditButtonClick() {
-        onView(withId(R.id.profileImg))
-            .check(matches(isDisplayed()))
-
-        onView(withId(R.id.editBtn))
-            .check(matches(isDisplayed()))
-            .perform(click())
-
-    }
-
-    @Test
-    fun testNameInputField() {
+    fun testInputFieldsInteraction() {
+        // Test Name Input
         val testName = "John Doe"
-
-        onView(withId(R.id.nameInput))
-            .check(matches(isDisplayed()))
+        onView(withId(R.id.NameInput))
             .perform(typeText(testName), closeSoftKeyboard())
-
-        onView(withId(R.id.nameInput))
             .check(matches(withText(testName)))
-    }
 
-    @Test
-    fun testUsernameInputField() {
+        // Test Username Input
         val testUsername = "johndoe123"
-
-        onView(withId(R.id.usernameInput))
-            .check(matches(isDisplayed()))
+        onView(withId(R.id.UsernameInput))
             .perform(typeText(testUsername), closeSoftKeyboard())
-
-        onView(withId(R.id.usernameInput))
             .check(matches(withText(testUsername)))
-    }
 
-    @Test
-    fun testPhoneInputField() {
+        // Test Phone Input
         val testPhone = "1234567890"
-
-        onView(withId(R.id.phoneInput))
-            .check(matches(isDisplayed()))
+        onView(withId(R.id.PhoneInput))
             .perform(typeText(testPhone), closeSoftKeyboard())
-
-        onView(withId(R.id.phoneInput))
             .check(matches(withText(testPhone)))
     }
 
     @Test
-    fun testDateInputField() {
-        val testDate = "01/01/1990"
+    fun testDatePickerInteraction() {
+        // Open date picker
+        onView(withId(R.id.DateInput)).perform(click())
 
-        onView(withId(R.id.dateInput))
-            .check(matches(isDisplayed()))
-            .perform(typeText(testDate), closeSoftKeyboard())
 
-        onView(withId(R.id.dateInput))
-            .check(matches(withText(testDate)))
     }
 
     @Test
-    fun testAddressSpinnerSelection() {
-        val testAddress = "Kathmandu"
+    fun testProfileImageUpload() {
 
-        onView(withId(R.id.addressSpinner))
-            .check(matches(isDisplayed()))
-            .perform(click())
-        onView(withText(testAddress))
-            .perform(click())
-
-
-        onView(withId(R.id.addressSpinner))
-            .check(matches(withSpinnerText(testAddress)))
+        onView(withId(R.id.ImageBtn)).perform(click())
     }
 
-    @Test
-    fun testContinueButtonClick() {
-        onView(withId(R.id.continueBtnPD))
-            .check(matches(isDisplayed()))
-            .perform(click())
 
-
-    }
 }
